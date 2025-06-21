3d6bf3e171c8a1de0ddbd153fcbb003d9393faff
Maryadajain <maryadajain1304@gmail.com>
2025-06-21 12:58:06 +0530

    DSA

--- DNF012.cpp
+++ DNF012.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void sortDNF012(int arr[], int n){
+void sortDNF012(int arr[], int n){  // O(n) time complexity, O(1) space complexity , Single pass
     int low =0, mid = 0, high = n-1;
     while(mid <= high){
         if(arr[mid]==0){ //0
