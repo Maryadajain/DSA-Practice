84fdcca296d2f8381b9d0c34789a8ca478b6331b
Maryadajain <maryadajain1304@gmail.com>
2025-07-08 00:04:41 +0530

    DSA

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1; // Base case
+    }
+    return n * factorial(n - 1); // Recursive call
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n; 
+    cout << "Factorial of " << n << " is " << factorial(n) << endl; // Call the recursive function
+    return 0; 
+}
