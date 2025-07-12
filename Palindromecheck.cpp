9ec2d14605923bb930762e2b863658d493d518ec
Maryadajain <maryadajain1304@gmail.com>
2025-07-12 12:47:49 +0530

    DSA

--- Palindromecheck.cpp
+++ Palindromecheck.cpp
@@ -2,17 +2,18 @@
 #include<string>
 using namespace std;
 
-bool isPalindrome(string str){
-    int left=0;
-    int right= str.length()-1;
+bool isPalindrome(string str) {
+    int left = 0;
+    int right = str.length() - 1;
 
-    while(left<right){
-        if(str[left]!=str[right]){
-            return false; // Not a palindrome
-        } else return true; // Is a palindrome
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false; // Characters don't match => Not a palindrome
+        }
         left++;
         right--;
     }
+    return true; // All characters matched => It's a palindrome
 }
 
 int main(){
